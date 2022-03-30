297dd52e93a43d991ed2d0fd3390d63414b2db67
saadmh902 <49423626+saadmh902@users.noreply.github.com>
2022-03-29 20:55:29 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,7 +1,8 @@
 import requests
 import time
 import sys
-from directories import scanDirectories
+from directories import scanDirectories,checkForFTP,checkForSSH
+from xss import scanXSS
 
 
 #####################################################################
@@ -16,23 +17,39 @@ from directories import scanDirectories
 # I am not liable for any damage you cause!
 
 
-print("Choose one of the following options")
-print("-------------------------------------")
-print("1:\tScan for directories")
-print("2:\tScan for XSS")
-print("3:\tScan for insecure GET paramaters")
-print("-------------------------------------")
-print("\n")
-
-
-
 #scanDirectories("http://localhost/","y")
 
 
 while(True):
+	print("Choose one of the following options\t")
+	print("-------------------------------------")
+	print("1:\tScan for directories")
+	print("2:\tScan for directories with DirBuster list (Long)")
+	print("3:\tScan for XSS")
+	print("4:\tScan for insecure GET parameters")
+	print("5:\tBrute force SSH")
+	print("6:\tBrute force FTP")
+	print("q:\tExit")
+	print("-------------------------------------")
+	print("\n")
 	chooseOption = input("Choose option:")
 	if(chooseOption == "1"):
-		url = input("Enter base website url ('E.g. http://google.com/': ")
-		scanDirectories(url,"n")
+		url = input("Enter base website url ('E.g. http://google.com/'): ")
+		#url="http://localhost/minecraftlookup/"
+		scanDirectories(url,"n",0)
+	elif(chooseOption == "2"):
+		url = input("Enter base website url ('E.g. http://google.com/'): ")
+		scanDirectories(url,"n",1)
+	elif(chooseOption == "5"):
+		url = input("URL: ")
+		checkForSSH(url)
+	elif(chooseOption == "6"):
+		url = input("URL: ")
+		print("0:\tCheck against simple credential list (Quick)")
+		print("1:\tCheck against large credential list (Long)")
+		search_level = int(input("Choose Selection:"))
+		checkForFTP(url,search_level)
+	elif(chooseOption == "q"):
+		exit()
 	else:
 		print("Invalid option. try again!")
