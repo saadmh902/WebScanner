0001771b93b3c5efcdf14d7179bc26ad0367fbc6
saadmh902 <49423626+saadmh902@users.noreply.github.com>
2022-03-31 23:30:54 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,8 +1,15 @@
 import requests
 import time
 import sys
-from directories import scanDirectories,checkForFTP,checkForSSH
+from directories import scanDirectories,checkForFTP,checkForSSH,checkForSFTP
 from xss import scanXSS
+from colorama import Fore, Back, Style,init
+import re
+init(convert=True)
+from urllib.parse import urlparse
+
+
+
 
 
 #####################################################################
@@ -19,7 +26,33 @@ from xss import scanXSS
 
 #scanDirectories("http://localhost/","y")
 
-
+#print(Fore.GREEN + "test" + Style.RESET_ALL)
+def extras():
+	while(True):
+		print("Extras:")
+		print("-------------------------------------")
+		print("0:\tMain Menu")
+		print("5:\tBrute force SSH")
+		print("6:\tBrute force FTP")
+		print("7:\tBrute force SFTP")
+		print("-------------------------------------")
+		chooseOption = input("Choose option")
+		if(chooseOption == "5"):
+			url = input("URL: ")
+			checkForSSH(url)
+		elif(chooseOption == "6"):
+			url = input("URL: ")
+			print("0:\tCheck against simple credential list (Quick)")
+			print("1:\tCheck against large credential list (Long)")
+			search_level = int(input("Choose Selection:"))
+			checkForFTP(url,search_level)
+		elif(chooseOption == "7"):
+			url = input("URL: ")
+			checkForSFTP(url)
+		elif(chooseOption == "0"):
+			break
+		else:
+			print("Invalid Option")
 while(True):
 	print("Choose one of the following options\t")
 	print("-------------------------------------")
@@ -27,29 +60,25 @@ while(True):
 	print("2:\tScan for directories with DirBuster list (Long)")
 	print("3:\tScan for XSS")
 	print("4:\tScan for insecure GET parameters")
-	print("5:\tBrute force SSH")
-	print("6:\tBrute force FTP")
+	print("5:\tExtras")
 	print("q:\tExit")
 	print("-------------------------------------")
 	print("\n")
-	chooseOption = input("Choose option:")
+	chooseOption = input("Choose option: ")
 	if(chooseOption == "1"):
-		url = input("Enter base website url ('E.g. http://google.com/'): ")
+		url = input("Enter base website url (e.g. http://google.com/): ")
 		#url="http://localhost/minecraftlookup/"
 		scanDirectories(url,"n",0)
 	elif(chooseOption == "2"):
-		url = input("Enter base website url ('E.g. http://google.com/'): ")
+		url = input("Enter base website url (e.g. http://google.com/): ")
 		scanDirectories(url,"n",1)
-	elif(chooseOption == "5"):
-		url = input("URL: ")
-		checkForSSH(url)
-	elif(chooseOption == "6"):
-		url = input("URL: ")
-		print("0:\tCheck against simple credential list (Quick)")
-		print("1:\tCheck against large credential list (Long)")
-		search_level = int(input("Choose Selection:"))
-		checkForFTP(url,search_level)
+	elif(chooseOption == "3"):
+		print("Not currently available")
+	elif(chooseOption == "4"):
+		print("Not currently available")
 	elif(chooseOption == "q"):
 		exit()
+	elif(chooseOption=="5"):
+		extras()
 	else:
 		print("Invalid option. try again!")
